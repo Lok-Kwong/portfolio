6658e1e1bd0d9819ee11886ca9f8875d52b237fb
Lok <hk65962@uga.edu>
2025-04-07 22:14:43 -0400

    Added bulk of what I wanted

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react';
+import kirby from './assets/Parasol_Kirby.webp'
+import cloud from './assets/cloud.png'
+import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const parallax = useRef<IParallax>(null!)
+  const [count, setCount] = useState(0);
 
+  const handleClick = (count: number) => {
+    if (count == 2) setCount(0);
+    else setCount(count => count + 1);
+    parallax.current.scrollTo(count)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <div style={{ width: '100%', height: '100%', background: '#253237' }}>
+    <Parallax ref={parallax} pages={3}>
+      <ParallaxLayer offset={0} speed={0} factor={3} className='background'/>
+      <ParallaxLayer offset={1} speed={1}/>
+      <ParallaxLayer offset={2} speed={1}/>
+
+      <ParallaxLayer offset={0} speed={0.05}>  
+        <div className='typewritter'>
+          <h2>Welcome to Lok's website!</h2>
+        </div>
+      </ParallaxLayer>
+
+      <ParallaxLayer sticky={{ start: 0.05, end: 3.5 }} onClick={() => handleClick(count)}>
+        <img src={kirby} style={{ width: '15%', marginLeft: '80%' }} />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.95 }}>
+        <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+        <img src={cloud} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.95 }}>
+        <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+        <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.95 }}>
+        <img src={cloud} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+        <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: .95 }}>
+        <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+        <img src={cloud} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+        <img src={cloud} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.95 }}>
+        <img src={cloud} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+        <img src={cloud} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+      </ParallaxLayer>
+
+    </Parallax>
+  </div>
+  );
 }
 
-export default App
+export default App;
